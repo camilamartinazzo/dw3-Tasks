a14ba0d9849f2df70b7e717744cedd0bc07a1157
camilamartinazzo <camilarichter2000@gmail.com>
2017-02-23 20:09:19 -0300

    Add files via upload

---
+++ desafio.java
@@ -0,0 +1,13 @@
+public class exDesafio {
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 1;
+		System.out.println(a);
+		System.out.println(b);
+		for (int cont = 2; cont <= 100; cont++){
+			System.out.println(a+b);
+			a = a+b;
+			b = a-b;
+		}
+	}
+}
---
+++ ex7.java
@@ -0,0 +1,11 @@
+public class ex7 {
+	public static void main(String[] args, int x) {
+		while (x>=1){
+			if (x%2 == 0){
+				x = x/2;
+			} else {
+				x = 3 * x + 1;
+			}
+		}
+	}
+}
